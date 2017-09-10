f66d9b4de3bb39ce96d5d65ce4cc88328604e625
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-10 00:01:22 -0400

    Get and Set function

--- HelloWorldContract.sol
+++ HelloWorldContract.sol
@@ -2,4 +2,13 @@ pragma solidity ^0.4.0;
 
 contract HelloWorldContract {
   string word = "Hello World";
+
+  function getWord() constant returns(string){
+    return word;
+    }
+
+  function setWord(string newWord) returns(string) {
+    word = newWord;
+    return word;
+  }
 }
