ee6fd09e9eaeea930bcd574e2d55e68979bf69b7
Aaron Anderson <aaronlynanderson@gmail.com>
2017-09-10 00:22:14 -0400

    Restrict non-creator addresses from modifications

--- HelloWorldContract.sol
+++ HelloWorldContract.sol
@@ -2,13 +2,26 @@ pragma solidity ^0.4.0;
 
 contract HelloWorldContract {
   string word = "Hello World";
+  address issuer;
+
+  function HelloWorldContract(){
+    issuer = msg.sender;
+  }
+
+  modifier ifIssuer(){
+      if(issuer != msg.sender){
+          throw;
+      } else{
+          _;
+      }
+  }
 
   function getWord() constant returns(string){
     return word;
     }
 
   function setWord(string newWord) returns(string) {
-    word = newWord;
-    return word;
+     word = newWord;
+     return "This is the creator!";
   }
 }
